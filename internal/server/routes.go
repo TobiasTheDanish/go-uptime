e5f1f9e931731520c1c8fa8230e2b27c4e5b6e4b
THCSC <tobias@skancode.dk>
2024-06-16 22:30:35 +0200

    update handler to produce blocks of up and down time

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -10,17 +10,65 @@ import (
 func (s *Server) RegisterRoutes() http.Handler {
 	e := echo.New()
 	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
+	// e.Use(middleware.Recover())
 
-	e.GET("/", s.HelloWorldHandler)
+	e.GET("/", s.GetStatusHandler)
 
 	return e
 }
 
-func (s *Server) HelloWorldHandler(c echo.Context) error {
-	resp := map[string]string{
-		"message": "Hello World",
+type State struct {
+	State                 string
+	StartTimeMS           int64
+	DurationMS            int64
+	AverageResponseTimeMs int64
+}
+
+func (s *Server) GetStatusHandler(c echo.Context) error {
+	stateMap := make(map[string][]State)
+
+	for url, statusSlice := range StatusMap {
+		stateMap[url] = make([]State, 0)
+		if len(statusSlice) == 0 {
+			break
+		}
+
+		var prevUp bool
+		var currentState State
+		count := 1
+		responseTimes := int64(statusSlice[0].ReponseTime)
+
+		for _, status := range statusSlice {
+			if currentState == (State{}) || status.IsUp != prevUp {
+				if currentState != (State{}) {
+					currentState.DurationMS = status.Time - currentState.StartTimeMS
+					currentState.AverageResponseTimeMs = responseTimes / int64(count)
+					stateMap[url] = append(stateMap[url], currentState)
+					count = 1
+					responseTimes = status.ReponseTime
+				}
+
+				currentState = State{
+					State:       isUpString(status.IsUp),
+					StartTimeMS: status.Time,
+				}
+			} else {
+				responseTimes += status.ReponseTime
+				count += 1
+			}
+			prevUp = status.IsUp
+		}
+		currentState.DurationMS = statusSlice[len(statusSlice)-1].Time - currentState.StartTimeMS
+		currentState.AverageResponseTimeMs = responseTimes / int64(count)
+		stateMap[url] = append(stateMap[url], currentState)
 	}
 
-	return c.JSON(http.StatusOK, resp)
+	return c.JSON(http.StatusOK, stateMap)
+}
+
+func isUpString(isUp bool) string {
+	if isUp {
+		return "Up"
+	}
+	return "Down"
 }
